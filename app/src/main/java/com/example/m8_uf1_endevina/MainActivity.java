6e7886deaea99d2808e9085f984351caf7712caf
DavidIbanezMoya <davidibanezmoya@gmail.com>
2022-09-16 19:20:19 +0200

    commit2 it verifies the number

--- app/src/main/java/com/example/m8_uf1_endevina/MainActivity.java
+++ app/src/main/java/com/example/m8_uf1_endevina/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.m8_uf1_endevina;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    int number = 22;
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.comprovar);
+        TextView input = (TextView) findViewById(R.id.userNumber);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                Context context = getApplicationContext();
+                int userGuess = Integer.valueOf(input.getText().toString());
+                //Log.v("Algo","Este sera el numero"+R.id.userNumber);
+                if (userGuess == number) {
+                    Toast.makeText(context, "Correct, you have guessed it right", Toast.LENGTH_SHORT).show();
+                }
+                if (userGuess < number) {
+                    Toast.makeText(context, "The number has to be bigger!", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(context, "The number has to be smaller!", Toast.LENGTH_SHORT).show();
+
+                    //Toast.makeText(context, "The number has to be smaller!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
 }
