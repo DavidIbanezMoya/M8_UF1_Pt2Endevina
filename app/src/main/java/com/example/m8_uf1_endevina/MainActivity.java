d041891c2fafef9ed7df519b95c3d5e802e8ab93
DavidIbanezMoya <davidibanezmoya@gmail.com>
2022-09-30 19:16:23 +0200

    commit4 It generates a dialog, when you guess the number + other stuff

--- app/src/main/java/com/example/m8_uf1_endevina/MainActivity.java
+++ app/src/main/java/com/example/m8_uf1_endevina/MainActivity.java
@@ -1,18 +1,22 @@
 package com.example.m8_uf1_endevina;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    int number = (int) Math.floor(Math.random() * (99 - 1 + 1) + 1);
+    int number = (int) Math.floor(Math.random() * (100 - 1 + 1) + 1);
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
@@ -22,6 +26,7 @@ public class MainActivity extends AppCompatActivity {
         TextView numer = (TextView) findViewById(R.id.numero);
         TextView registry = (TextView) findViewById(R.id.Registry);
         TextView logs = (TextView) findViewById(R.id.Logs);
+
         button.setOnClickListener(new View.OnClickListener() {
             int userTry = 0;
 
@@ -33,9 +38,11 @@ public class MainActivity extends AppCompatActivity {
                 Log.v("Intento","Intento numero: "+userTry);
                 //Log.v("Algo","Este sera el numero"+R.id.userNumber);
                 if (userGuess == number) {
+
                     Toast.makeText(context, "Correct, you have guessed it right", Toast.LENGTH_SHORT).show();
+                    showDialog();
                 }
-                if (userGuess < number) {
+                else if (userGuess < number) {
                     Toast.makeText(context, "The number has to be bigger!", Toast.LENGTH_SHORT).show();
                 }
                 else {
@@ -49,4 +56,29 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    //En void no hi ha problema pero no se com recuperar el nom que agafem de l usuari
+ public EditText showDialog(){
+        //Para anadir el dialogo donde el usuario decidira si volver a jugar una nueva partida o no
+     //Y que pueda guardar el campo del nombre de usuario
+     AlertDialog.Builder builder = new AlertDialog.Builder(this);
+     String message = "Congratulations";
+    final EditText name = new EditText(this);
+
+     builder.setView(name).setMessage(message).setPositiveButton("Play again", new DialogInterface.OnClickListener() {
+
+         public void onClick(DialogInterface dialogInterface, int i) {
+            number = (int) Math.floor(Math.random() * (100 - 1 + 1) + 1);
+            //Les altres variables s han de posar globals com aquesta
+         }
+     }).setNegativeButton("Save and close", new DialogInterface.OnClickListener() {
+
+         public void onClick(DialogInterface dialogInterface, int i) {
+
+         }
+     });
+
+      builder.create().show();
+      return name;
+ }
 }
+
